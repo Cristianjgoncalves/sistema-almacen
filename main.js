5388847650e2a7cb567b4a2fb8a334eea2b17193
Cristianjgoncalves <162377704+Cristianjgoncalves@users.noreply.github.com>
2026-01-29 22:58:06 -0300

    Add files via upload
    primer vercion

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    title: "El Almacén del Repostero",
+    icon: path.join(__dirname, 'icon.ico'), // Opcional si tienes icono
+    autoHideMenuBar: true, // Esto oculta el menú "File, Edit, etc."
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
